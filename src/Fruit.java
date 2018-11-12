02076ea27ca3185b6b2db95294e8bc1ff6780576
Ken <kjaggi94@gmail.com>
2018-11-12 22:05:26 +0100

    refactor(fruit): add setter and rename variable in constructor

--- src/Fruit.java
+++ src/Fruit.java
@@ -4,23 +4,34 @@ public class Fruit implements Element {
     private float weight;
     private String name;
 
-    public Fruit(double priceKg, float wt, String nm){
+    public Fruit(double priceKg, float weight, String name){
         this.pricePerKg = priceKg;
-        this.weight = wt;
-        this.name = nm;
+        this.weight = weight;
+        this.name = name;
     }
 
     public double getPricePerKg() {
         return pricePerKg;
     }
 
+    public void setPricePerKg(double pricePerKg){
+        this.pricePerKg = pricePerKg;
+    }
 
     public float getWeight() {
         return weight;
     }
 
+    public void setWeight(float weight){
+        this.weight = weight;
+    }
+
     public String getName(){
         return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
 
     @Override
